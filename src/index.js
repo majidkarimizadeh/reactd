2f74e7b97990f9c88921a87e03ed12e4ff15bee8
majid karimi zadeh <mj.karimizadeh@gmail.com>
2018-11-14 17:47:14 +0330

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'babel-polyfill';
+import App from './App';
+//import registerServiceWorker from './registerServiceWorker';
+import { Router } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop';
+import history from './history'
+
+ReactDOM.render(
+    <Router history={history}>
+        <ScrollToTop>
+            <App></App>
+        </ScrollToTop>
+    </Router>,
+    document.getElementById('root')
+);
+
+//registerServiceWorker();
