b02714ca9868d1b7818867ab3a0d467c22615490
majid karimi zadeh <mj.karimizadeh@gmail.com>
2018-12-17 14:54:03 +0330

    open in local host

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
-// export const SITE_URL = 'http://localhost:8000/'
-export const SITE_URL = 'http://192.168.2.26:8000'
+export const SITE_URL = 'http://localhost:8000/'
+//export const SITE_URL = 'http://192.168.2.26:8000'
 export const API_URL = SITE_URL + '/api/v1'
 
 export const WYSIWYG_UPLOAD_URL = API_URL + '/wysiwyg/upload'
