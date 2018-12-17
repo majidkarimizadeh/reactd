3d00306174d7ecf5a324719b30e553eca52c0a9e
majid karimi zadeh <mj.karimizadeh@gmail.com>
2018-12-17 14:58:09 +0330

    fix opening in localhost

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const SITE_URL = 'http://localhost:8000/'
+export const SITE_URL = 'http://localhost:8000'
 //export const SITE_URL = 'http://192.168.2.26:8000'
 export const API_URL = SITE_URL + '/api/v1'
 
