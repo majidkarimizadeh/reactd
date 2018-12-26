657bc8a3f8a49658d403ff9f5876863b943c4d57
majid karimi zadeh <mj.karimizadeh@gmail.com>
2018-12-26 12:56:49 +0330

    better error bundry

--- src/utils/errorBoundary.js
+++ src/utils/errorBoundary.js
@@ -16,17 +16,29 @@ export default class ErrorBoundary extends Component {
       		error: error,
       		errorInfo: errorInfo
     	})
+
+        if(error.status && error.status === 401) 
+        {
+            history.push('/login')
+        }
   	}
   
   	render() {
   		const { error } = this.state 
+
     	if (error) {
+
       		return (
 	        	<div className='error-page'>
 	          		<div>
 	          			<h1 className='status'>{error.status}</h1>
 	          			<div className='message'>
-	          				<div>{error.data.error}</div>
+	          				<div>
+                                {error.data ? error.data.error : ''}
+                            </div>
+                            <div>
+                                {(error.response && error.response.data) ? error.response.data.error : ''}
+                            </div>
 	          				<div>
                                 {(error.status === 401) && 
                                     <a onClick={() => history.push('/login')} className='back'>
